5285b53eb195d8a1750fd2315e5733a2aff98027
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-03-30 12:44:43 +0700

    update

--- jscroot/view/content/proyek/assessment.js
+++ jscroot/view/content/proyek/assessment.js
@@ -79,7 +79,7 @@ function handleActivityScoreResponse(result) {
     console.log({result});
     if (result.status === 200) {
         updateTableRow(1, result.data.stravakm, result.data.strava);
-        updateTableRow(2, result.data.iq, result.data.iqresult);
+        updateTableRow(2, result.data.iqresult, result.data.iq);
         // updateTableRow(3, result.data.pomokitsesi, result.data.pomokit);
         updateTableRow(6, result.data.trackerdata, result.data.tracker);
         // updateTableRow(9, result.data.gtmetrixresult, result.data.gtmetrix);
