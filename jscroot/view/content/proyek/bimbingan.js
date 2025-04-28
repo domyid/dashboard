5bd81fd4cbff83d6bd5933a88a6352e744022861
Febriand1 <fdirga63@gmail.com>
2025-04-28 13:45:03 +0700

    coba

--- jscroot/view/content/proyek/bimbingan.js
+++ jscroot/view/content/proyek/bimbingan.js
@@ -1,79 +1,37 @@
-import {
-    onClick,
-    getValue,
-    setValue,
-    onInput,
-    hide,
-    show,
-} from 'https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js';
-import { validatePhoneNumber } from 'https://cdn.jsdelivr.net/gh/jscroot/validate@0.0.2/croot.js';
-import {
-    postJSON,
-    getJSON,
-} from 'https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js';
-import { getCookie } from 'https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js';
-import { addCSSIn } from 'https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js';
+import { onClick,getValue,setValue,onInput,onChange,hide,show } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js";
+import {validatePhoneNumber} from "https://cdn.jsdelivr.net/gh/jscroot/validate@0.0.2/croot.js";
+import {postJSON,getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
+import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
+import {addCSSIn} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
-import { id, backend } from '/dashboard/jscroot/url/config.js';
+import { id,backend } from "/dashboard/jscroot/url/config.js";
 
-export async function main() {
+export async function main(){    
     onInput('phonenumber', validatePhoneNumber);
-    await addCSSIn(
-        'https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css',
-        id.content
-    );
-    getJSON(
-        backend.project.data,
-        'login',
-        getCookie('login'),
-        getResponseFunction
-    );
-    getJSON(
-        backend.project.assessment,
-        'login',
-        getCookie('login'),
-        getBimbinganList
-    );
+    await addCSSIn("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css",id.content);
+    getJSON(backend.project.data,'login',getCookie('login'),getResponseFunction);
+    getJSON(backend.project.assessment,'login',getCookie('login'),getBimbinganList);
     onClick('tombolmintaapproval', actionfunctionname);
+    onChange('bimbingan-name', handleBimbinganChange);
     fetchActivityScore();
 }
 
-function actionfunctionname() {
-    let idprjusr = {
-        // _id: getValue('project-name'),
-        asesor: {
-            phonenumber: getValue('phonenumber'),
-        },
-    };
-    if (getCookie('login') === '') {
-        redirect('/signin');
-    } else {
-        const bimbinganLanjutan = backend.project.assessment + '/lanjutan';
-        postJSON(
-            bimbinganLanjutan,
-            'login',
-            getCookie('login'),
-            idprjusr,
-            postResponseFunction
-        );
-        // hide("tombolbuatproyek");
-    }
-}
+function handleBimbinganChange(target) {
+    const selectedOptionValue = target.value;
+    const selectedOption = document.querySelector(
+        `#bimbingan-name option[value="${selectedOptionValue}"]`
+    );
 
-function getResponseFunction(result) {
-    if (result.status === 200) {
-        result.data.forEach((project) => {
-            const option = document.createElement('option');
-            option.value = project._id;
-            option.textContent = project.name;
-            document.getElementById('project-name').appendChild(option);
-        });
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: result.data.status,
-            text: result.data.response,
-        });
+    if (selectedOption) {
+        const selectedBimbinganKe = selectedOption.dataset.bimbinganke;
+
+        if (selectedBimbinganKe) {
+            const url = `${backend.project.assessment}?bimbinganke=${selectedBimbinganKe}`;
+            console.log('Fetching data untuk:', url);
+            getJSON(url, 'login', getCookie('login'), handleActivityScoreResponse);
+        } else {
+            console.warn('Bimbingan ke- tidak ditemukan pada option.');
+        }
     }
 }
 
@@ -83,11 +41,10 @@ function getBimbinganList(result) {
         result.data.forEach((bimbingan) => {
             const option = document.createElement('option');
             option.value = bimbingan._id;
-            option.textContent =
-                bimbingan.bimbinganke === undefined
-                    ? 'Bimbingan ke-1'
-                    : 'Bimbingan ke-' + bimbingan.bimbinganke;
 
+            const bimbinganText = 'Bimbingan Ke-';
+            option.textContent = bimbinganText + (bimbingan.bimbinganke ?? 1);
+            option.dataset.bimbinganke = bimbingan.bimbinganke ?? 1;
             document.getElementById('bimbingan-name').appendChild(option);
         });
     } else {
@@ -99,8 +56,42 @@ function getBimbinganList(result) {
     }
 }
 
-function postResponseFunction(result) {
-    if (result.status === 200) {
+function actionfunctionname(){
+    let idprjusr = {
+        // _id: getValue('project-name'),
+        asesor: {
+            phonenumber: getValue('phonenumber'),
+        },
+    };
+    if (getCookie("login")===""){
+        redirect("/signin");
+    }else{
+        const bimbinganLanjutan = backend.project.assessment + "/lanjutan"
+        postJSON(bimbinganLanjutan,"login",getCookie("login"),idprjusr,postResponseFunction);
+        // hide("tombolbuatproyek");    
+    }
+}
+
+function getResponseFunction(result){
+    if (result.status===200){
+        result.data.forEach(project => {
+            const option = document.createElement('option');
+            option.value = project._id;
+            option.textContent = project.name;
+            document.getElementById('project-name').appendChild(option);
+        });
+
+    }else{
+        Swal.fire({
+            icon: "error",
+            title: result.data.status,
+            text: result.data.response,
+          });
+    }
+}
+
+function postResponseFunction(result){
+    if(result.status === 200){
         // const katakata = "Selamat! Anda telah berhasil mengajukan permohonan penilaian proyek. Silakan tunggu konfirmasi dari asesor.";
         Swal.fire({
             icon: 'success',
@@ -110,7 +101,7 @@ function postResponseFunction(result) {
                 setValue('phonenumber', '');
             },
         });
-    } else if (result.data.status.startsWith('Info : ')) {
+    }else if (result.data.status.startsWith("Info : ")) {
         Swal.fire({
             icon: 'info',
             title: result.data.status,
@@ -128,12 +119,7 @@ function postResponseFunction(result) {
 }
 
 function fetchActivityScore() {
-    getJSON(
-        backend.activityscore.weekly,
-        'login',
-        getCookie('login'),
-        handleActivityScoreResponse
-    );
+    getJSON(backend.activityscore.weekly, 'login', getCookie('login'), handleActivityScoreResponse);
 }
 
 function handleActivityScoreResponse(result) {
@@ -143,22 +129,15 @@ function handleActivityScoreResponse(result) {
         updateTableRow(1, result.data.stravakm, result.data.strava);
         updateTableRow(2, result.data.iqresult, result.data.iq);
         updateTableRow(3, result.data.pomokitsesi, result.data.pomokit);
-        updateTableRow(
-            4,
-            result.data.mbc,
-            result.data.mbcPoints || result.data.blockchain
-        );
-        updateTableRow(
-            5,
-            result.data.rupiah,
-            result.data.qrisPoints || result.data.qris
-        );
+        updateTableRow(4, result.data.mbc, result.data.mbcPoints || result.data.blockchain); 
+        updateTableRow(5, result.data.rupiah, result.data.qrisPoints || result.data.qris);     
         updateTableRow(6, result.data.trackerdata, result.data.tracker);
         updateTableRow(7, result.data.bukukatalog, result.data.bukped);
         updateTableRow(9, result.data.gtmetrixresult, result.data.gtmetrix);
         updateTableRow(10, result.data.webhookpush, result.data.webhook);
         updateTableRow(11, result.data.presensihari, result.data.presensi);
         updateTableRow(12, result.data.rvn, result.data.ravencoinPoints || 0);
+
     } else {
         console.log(result.data.message);
     }
