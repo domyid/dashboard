2bce85884271f44ec366cff6ba4126fcb1aa762e
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-05-01 16:59:20 +0700

    update js

---
+++ jscroot/view/content/proyek/kelasws.js
@@ -0,0 +1,116 @@
+import { onClick,getValue,setValue,onInput,onChange,hide,show } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js";
+import {postJSON,getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
+import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
+import {addCSSIn} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
+import { id,backend } from "/dashboard/jscroot/url/config.js";
+
+export async function main(){
+    await addCSSIn("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css",id.content);
+    getJSON(backend.project.data,'login',getCookie('login'),getResponseFunction);
+    onClick('tombolkirimtugas', actionfunctionname);
+    // fetchActivityScore();
+}
+
+function actionfunctionname(){
+    let idprjusr = {
+        kelas: getValue('kelas-name'),
+        alltugas: [
+            getValue('tugas1'),
+            getValue('tugas2'),
+        ],
+    };
+    if (getCookie("login")===""){
+        redirect("/signin");
+    }else{
+        const tugaskelasws = backend.project.kelasws
+        postJSON(tugaskelasws,"login",getCookie("login"),idprjusr,postResponseFunction);
+        // hide("tombolbuatproyek");    
+    }
+}
+
+function getResponseFunction(result){
+    if (result.status===200){
+        result.data.forEach(project => {
+            const option = document.createElement('option');
+            option.value = project._id;
+            option.textContent = project.name;
+            document.getElementById('project-name').appendChild(option);
+        });
+
+    }else{
+        Swal.fire({
+            icon: "error",
+            title: result.data.status,
+            text: result.data.response,
+          });
+    }
+}
+
+function postResponseFunction(result){
+    if(result.status === 200){
+        // const katakata = "Selamat! Anda telah berhasil mengajukan permohonan penilaian proyek. Silakan tunggu konfirmasi dari asesor.";
+        Swal.fire({
+            icon: 'success',
+            title: 'Berhasil',
+            text: 'Selamat! Anda telah berhasil mengajukan permohonan penilaian proyek. Silakan tunggu konfirmasi dari asesor.',
+            didClose: () => {
+                setValue('kelas-name', '');
+                setValue('tugas1', '');
+                setValue('tugas2', '');
+            },
+        });
+    }else if (result.data.status.startsWith("Info : ")) {
+        Swal.fire({
+            icon: 'info',
+            title: result.data.status,
+            text: result.data.response,
+        });
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: result.data.status,
+            text: result.data.response,
+        });
+        // show("tombolmintaapproval");
+    }
+    console.log(result);
+}
+
+// function fetchActivityScore() {
+//     getJSON(backend.activityscore.weekly, 'login', getCookie('login'), handleActivityScoreResponse);
+// }
+
+// function handleActivityScoreResponse(result) {
+//     if (result.status === 200) {
+//         updateTableRow(0, result.data.sponsordata, result.data.sponsor);
+//         updateTableRow(1, result.data.stravakm, result.data.strava);
+//         updateTableRow(2, result.data.iqresult, result.data.iq);
+//         updateTableRow(3, result.data.pomokitsesi, result.data.pomokit);
+//         updateTableRow(4, result.data.mbc, result.data.mbcPoints || result.data.blockchain); 
+//         updateTableRow(5, result.data.rupiah, result.data.qrisPoints || result.data.qris);     
+//         updateTableRow(6, result.data.trackerdata, result.data.tracker);
+//         updateTableRow(7, result.data.bukukatalog, result.data.bukped);
+//         updateTableRow(9, result.data.gtmetrixresult, result.data.gtmetrix);
+//         updateTableRow(10, result.data.webhookpush, result.data.webhook);
+//         updateTableRow(11, result.data.presensihari, result.data.presensi);
+//         updateTableRow(12, result.data.rvn, result.data.ravencoinPoints || 0);
+
+//     } else {
+//         console.log(result.data.message);
+//     }
+// }
+
+// function updateTableRow(rowIndex, quantity, points) {
+//     const tableRows = document.querySelectorAll('table.table tbody tr');
+//     const row = tableRows[rowIndex]; // Ambil baris berdasarkan indeks
+//     if (row) {
+//         const quantityCell = row.querySelector('td:nth-child(3)');
+//         const pointsCell = row.querySelector('td:nth-child(4)');
+
+//         if (quantityCell && pointsCell) {
+//             quantityCell.textContent = quantity || 0;
+//             pointsCell.textContent = points || 0;
+//         }
+//     }
+// }
