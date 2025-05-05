6b60d92d7ba0d3b15bfbdfdea1306bbced1c3874
Febriand1 <fdirga63@gmail.com>
2025-05-05 19:33:10 +0700

    test

--- jscroot/view/content/proyek/kelasai1.js
+++ jscroot/view/content/proyek/kelasai1.js
@@ -1,55 +1,87 @@
-import { onClick,getValue,setValue,onInput,onChange,hide,show } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js";
-import {postJSON,getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
-import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
-import {addCSSIn} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import {
+    onClick,
+    getValue,
+    setValue,
+    onInput,
+    onChange,
+    hide,
+    show,
+} from 'https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js';
+import {
+    postJSON,
+    getJSON,
+} from 'https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js';
+import { getCookie } from 'https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js';
+import { addCSSIn } from 'https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js';
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
-import { id,backend } from "/dashboard/jscroot/url/config.js";
+import { id, backend } from '/dashboard/jscroot/url/config.js';
 
-export async function main(){
-    await addCSSIn("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css",id.content);
-    getJSON(backend.project.data,'login',getCookie('login'),getResponseFunction);
+export async function main() {
+    await addCSSIn(
+        'https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css',
+        id.content
+    );
+    getJSON(
+        backend.project.data,
+        'login',
+        getCookie('login'),
+        getResponseFunction
+    );
     onClick('tombolkirimtugas', actionfunctionname);
     fetchTugasScore();
 }
 
 const tugaskelasai = backend.project.kelasai + '1';
 
-function actionfunctionname(){
+function actionfunctionname() {
     let idprjusr = {
         kelas: getValue('kelas-name'),
-        alltugas: [
-            getValue('tugas1'),
-            getValue('tugas2'),
-        ],
+        alltugas: [getValue('tugas1'), getValue('tugas2')],
     };
-    if (getCookie("login")===""){
-        redirect("/signin");
-    }else{
-        postJSON(tugaskelasai,"login",getCookie("login"),idprjusr,postResponseFunction);
-        // hide("tombolbuatproyek");    
+    if (getCookie('login') === '') {
+        redirect('/signin');
+    } else {
+        postJSON(
+            tugaskelasai,
+            'login',
+            getCookie('login'),
+            idprjusr,
+            postResponseFunction
+        );
+        // hide("tombolbuatproyek");
     }
 }
 
-function getResponseFunction(result){
-    if (result.status===200){
-        result.data.forEach(project => {
+function getResponseFunction(result) {
+    if (result.status === 200) {
+        result.data.forEach((project) => {
             const option = document.createElement('option');
             option.value = project._id;
             option.textContent = project.name;
             document.getElementById('project-name').appendChild(option);
         });
-
-    }else{
+    } else {
         Swal.fire({
-            icon: "error",
+            icon: 'error',
             title: result.data.status,
             text: result.data.response,
-          });
+        });
     }
 }
 
-function postResponseFunction(result){
-    if(result.status === 200){
+function postResponseFunction(result) {
+    const id = target.value; // Ini _id nya
+    const defaultValue = 'x'.repeat(2);
+
+    if (id === defaultValue) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Gagal',
+            text: 'Silahkan pilih kelas yang benar',
+        });
+        return;
+    }
+    if (result.status === 200) {
         Swal.fire({
             icon: 'success',
             title: 'Berhasil',
@@ -58,7 +90,7 @@ function postResponseFunction(result){
                 setValue('kelas-name', '');
             },
         });
-    }else if (result.data.status.startsWith("Info : ")) {
+    } else if (result.data.status.startsWith('Info : ')) {
         Swal.fire({
             icon: 'info',
             title: result.data.status,
@@ -76,17 +108,29 @@ function postResponseFunction(result){
 }
 
 function fetchTugasScore() {
-    getJSON(tugaskelasai+'/weekly', 'login', getCookie('login'), handleTugasScoreResponse);
+    getJSON(
+        tugaskelasai + '/weekly',
+        'login',
+        getCookie('login'),
+        handleTugasScoreResponse
+    );
 }
 
 function handleTugasScoreResponse(result) {
-    console.log(result);
     if (result.status === 200) {
         updateTableRow(0, result.data.stravakm, result.data.strava);
         updateTableRow(1, result.data.iqresult, result.data.iq);
         updateTableRow(2, result.data.pomokitsesi, result.data.pomokit);
-        updateTableRow(3, result.data.mbc, result.data.mbcPoints || result.data.blockchain); 
-        updateTableRow(4, result.data.rupiah, result.data.qrisPoints || result.data.qris);
+        updateTableRow(
+            3,
+            result.data.mbc,
+            result.data.mbcPoints || result.data.blockchain
+        );
+        updateTableRow(
+            4,
+            result.data.rupiah,
+            result.data.qrisPoints || result.data.qris
+        );
         updateTableRow(5, result.data.rvn, result.data.ravencoinPoints || 0);
         addTableTugas(result.data.alltugas);
     } else {
