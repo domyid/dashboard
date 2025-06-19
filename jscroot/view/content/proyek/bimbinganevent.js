2d8b6ce691ebeb56a2261a0259a436ee9d33402a
“GilarYa” <gilarwahibul@gmail.com>
2025-06-20 04:09:18 +0700

    update js

--- jscroot/view/content/proyek/bimbinganevent.js
+++ jscroot/view/content/proyek/bimbinganevent.js
@@ -471,7 +471,7 @@ window.confirmClaim = function() {
                     showNotification(message, 'is-success');
                     closeClaimModal();
 
-                    // Refresh data
+                    // Refresh data - event akan hilang karena isactive = false
                     loadEvents();
                     loadUserClaims();
                 } else {
@@ -558,7 +558,7 @@ window.confirmSubmit = function() {
                 }
 
                 if (responseData.status === 'Success' || result.data?.status === 'Success') {
-                    showNotification('Tugas berhasil disubmit! Menunggu approval dari owner.', 'is-success');
+                    showNotification('Tugas sudah disubmit! Menunggu approval dari owner dalam 24 jam.', 'is-success');
                     closeSubmitModal();
 
                     // Refresh data
